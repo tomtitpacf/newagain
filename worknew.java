647a377c8c57bdc6fbdf791827ed3aa16d0f931f
sinit <sinitsynajuliana@gmail.com>
2023-12-21 12:58:08 +1400

    не чушпан

---
+++ worknew.java
@@ -0,0 +1,7 @@
+public class worknew {
+    //просто проверка
+    public static void main(String[] args) {
+        System.out.println("Что сказал программист, когда увидел некорректный код?");
+        System.out.println("WTF!");
+    }
+}
